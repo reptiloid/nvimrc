5193103434c86666113a4d71db86501019d37b31
Kirill K <2spybot@gmail.com>
2022-02-06 23:53:12 +0300

    initial commit

---
+++ lua/user/clever-f.lua
@@ -0,0 +1,7 @@
+
+-- vim.g.indentLine_enabled = 1
+-- vim.g.indent_blankline_show_trailing_blankline_indent = false
+
+
+vim.g.clever_f_across_no_line = 1
+vim.g.clever_f_ignore_case = 1
---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,21 @@
+vim.cmd [[
+try
+  " colorscheme darkplus
+  colorscheme badwolf
+
+  highlight Cursor guifg=white guibg=black
+  highlight iCursor guifg=white guibg=steelblue
+  set guicursor=n-v-c:block-Cursor
+  set guicursor+=i:ver100-iCursor
+  set guicursor+=n-v-c:blinkon0
+  set guicursor+=i:blinkwait10
+  set background=dark
+
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
+
+vim.g.badwolf_darkgutter = 1
+vim.g.badwolf_tabline = 1
