e6d4731dcabf4c9251a126a78255d4e21f517ef1
Kirill <2spybot@gmail.com>
2022-04-04 15:04:56 +0300

    last session added to alpha config

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -4,12 +4,24 @@ if not status_ok then
 end
 
 local function footer()
-  local plugins_count = vim.fn.len(vim.fn.globpath("c:\\Users\\admin\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start", "*", 0, 1))
-  local datetime = os.date("  %m-%d-%Y   %H:%M:%S")
-  local version = vim.version()
-  local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
-  return "    Plugins " .. plugins_count .. nvim_version_info
-  -- return datetime .. "   Plugins " .. plugins_count .. nvim_version_info
+    local plugins_count = 0
+    if vim.fn.has("win32") == 1 then
+        plugins_count = vim.fn.len(vim.fn.globpath("~/AppData/Local/nvim-data/site/pack/packer/start", "*", 0, 1))
+    else
+        plugins_count = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
+    end
+    local datetime = os.date(" %Y-%m-%d %H:%M:%S")
+    local version = vim.version()
+    local nvim_version_info = "   " .. version.major .. "." .. version.minor .. "." .. version.patch
+    return {
+        "",
+        "",
+        "",
+        "",
+        "",
+        " Plugins:    " .. plugins_count .. "    ",
+        " Neovim: " .. nvim_version_info,
+    }
 end
 
 local dashboard = require("alpha.themes.dashboard")
@@ -23,11 +35,13 @@ dashboard.section.header.val = {
 }
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("p", "  Projects", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
     dashboard.button("s", "  Sessions", ":SessionManager load_session<CR>"),
+    dashboard.button("l", "  Last Session", ":SessionManager load_last_session<CR>"),
     dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
 dashboard.section.footer.val = footer()
